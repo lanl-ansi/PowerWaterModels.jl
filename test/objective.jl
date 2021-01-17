b5c42a9778a914e407d4879a6208d30ce111b49e
Byron Tasseff <byron@tasseff.com>
2021-01-16 21:00:28 -0700

    Correct objective test water network path.

--- test/objective.jl
+++ test/objective.jl
@@ -1,12 +1,13 @@
 @testset "src/io/objective.jl" begin
     @testset "objective_min_max_generation_fluctuation" begin
         p_file = "$(pm_path)/test/data/matpower/case3.m"
-        w_file = "$(wm_path)/test/data/epanet/snapshot/pump-hw-lps.inp"
+        w_file = "$(wm_path)/test/data/epanet/multinetwork/pump-hw-lps.inp"
         pw_file = "../test/data/json/case3-pump.json"
 
         w_ext = Dict{Symbol,Any}(:pump_breakpoints => 3)
         p_type, w_type = LinDist3FlowPowerModel, PWLRDWaterModel
         p_data, w_data, pw_data = parse_files(p_file, w_file, pw_file)
+
         pm, wm = instantiate_model(p_data, w_data, pw_data, p_type, w_type, build_opwf; w_ext=w_ext)
         objective_min_max_generation_fluctuation(pm)
 
