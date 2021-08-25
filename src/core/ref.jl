6363e66040cc19baff0456502c9932e731b6d79d
Byron Tasseff <btasseff@lanl.gov>
2021-08-24 21:14:43 -0600

    Fix power conversion calculation.

--- src/core/ref.jl
+++ src/core/ref.jl
@@ -9,16 +9,19 @@ function _get_power_conversion_factor(pwm::AbstractPowerWaterModel; nw::Int = _I
         psf = data_pmd["nw"][string(nw)]["settings"]["power_scale_factor"]
         base_mva_pmd = sbase / psf
     end
-    
-    base_power_pmd = 1.0e-6 / base_mva_pmd # Watts per PM unit.
+
+    # Watts per PowerModelsDistribution power unit.
+    base_power_pmd = 1.0e6 * base_mva_pmd 
     
     # Get the conversion factor for power used by the water network.
     data_wm = _WM.get_wm_data(pwm.data)
     transform_mass = _WM._calc_mass_per_unit_transform(data_wm)
     transform_time = _WM._calc_time_per_unit_transform(data_wm)
     transform_length = _WM._calc_length_per_unit_transform(data_wm)
-    base_power_wm = transform_mass(1.0) * transform_length(1.0)^2 / transform_time(1.0)^3
+
+    # Scalar for WaterModels power units per Watt.
+    scalar_power_wm = transform_mass(1.0) * transform_length(1.0)^2 / transform_time(1.0)^3
 
     # Return the power conversion factor for pumps.
-    return base_power_wm / base_power_pmd
+    return (1.0 / scalar_power_wm) / base_power_pmd
 end
